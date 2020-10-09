14a926b18847f074014bc40003773eb1555b3702
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-10 11:56:50 +1300

    / getting todo data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 // ========== imports ==========
 import './default.js';
 
+// ========== references ==========
+
+
 // ========== script ==========
+// get todo
+class GetTodo {
+    constructor(colName) {
+        this.colName = colName;
+    }
+
+    get = function () {
+        firebase.firestore().collection(this.colName).get().then((cols) => {
+            cols.docs.forEach((doc) => {
+                console.log(doc.data());
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+}
+
+
+// main
+const main = function () {
+    // get todo
+    const colName = 'todos';
+    const getTodo = new GetTodo(colName);
+    getTodo.get();
+};
+
+main();
