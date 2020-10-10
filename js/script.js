2267028a42cf6c2d2a2cdc4c01b8c1ab510d082c
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-11 10:24:15 +1300

    / display todo / initialise app (get todo > display todo)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // ========== imports ==========
 import './default.js';
 
-// ========== references ==========
-
+// ========== DOM references ==========
+const todoGroup = document.querySelector('.todo-group');
 
 // ========== script ==========
 // get todo
@@ -34,17 +34,41 @@ class TodoGetter {
     }
 }
 
+// display todo
+class TodoDisplayer {
+    constructor(todos, todoGroup) {
+        this.todos = todos;
+        this.todoGroup = todoGroup;
+    }
+
+    display = function () {
+        this.todos.forEach(todo => {
+            this.todoGroup.innerHTML += `
+                <li class="todo-item">
+                    <p class="todo-text">${todo}</p>
+                    <button class="todo-delete-btn"><i class="fas fa-trash-alt todo-delete-icon"></i></button>
+                </li>
+            `;
+        });
+    }
+}
 
 // main
 const main = function () {
-    // get todo
-    const colName = 'todos';
-    const todoGetter = new TodoGetter(colName);
-    todoGetter.get().then(todos => {
-        console.log(todos);
-    }).catch(err => {
-        console.log(err);
-    });
+    // initialise
+    const init = function () {
+        // get todo
+        const colName = 'todos';
+        const todoGetter = new TodoGetter(colName);
+        todoGetter.get().then(todos => {
+            // display todo
+            const todoDisplayer = new TodoDisplayer(todos, todoGroup);
+            todoDisplayer.display();
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+    init();
 };
 
 main();
